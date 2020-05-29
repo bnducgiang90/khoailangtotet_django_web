b66c1b2e8051e642a35c50dabf80d1508b046fdc
trinh giang <bnducgiang90@gmail.com>
2020-05-29 08:21:25 +0700

    cm

--- apps/controllers/users.py
+++ apps/controllers/users.py
@@ -2,17 +2,25 @@ from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from databases.postgresqldb import postgresqldb
+from django.core.paginator import Paginator
 
 class usercontroller:
 
     @staticmethod
-    @login_required
+    #@login_required
     def index(request):
         template_name_index = 'users/index.html'
-        return render(request, template_name=template_name_index)
+        _db = postgresqldb()
+        _datas = _db.getdatas()
+        _paginator = Paginator(_datas, 2)
+        _page = request.GET.get('page')
+        _items = _paginator.get_page(_page)
+        _context = {'items': _items}
+        return render(request, template_name=template_name_index, context=_context)
 
     @staticmethod
-    @login_required
+    #@login_required
     def detail(request, user_id):
         template_name_detail = 'users/detail.html'
         context = {'context_detail': "Thông tin detail " + str(user_id)}
--- databases/postgresqldb.py
+++ databases/postgresqldb.py
@@ -1,24 +1,36 @@
+from collections import namedtuple
+
 from django.db import connections, connection, transaction
 from django.db.models.query import RawQuerySet
 from django.db.models.sql.query import RawQuery
-from psycopg2._psycopg import cursor, connection as pg_connection
+
+
+# from psycopg2._psycopg import cursor, connection as pg_connection
 
 
 class postgresqldb:
     def __init__(self):
-        self.__connection: pg_connection = None
-        self.__cursor: cursor = None
+        self.__connection = None
+        self.__cursor = None
 
     def connect(self):
-        self.__connection: pg_connection = connections['postgresql_khoailangdb']
+        self.__connection = connections['postgresql_khoailangdb']
         self.__cursor = self.__connection.cursor()
 
+    def getdatas(self):
+        with connections['postgresql_khoailangdb'].cursor() as _cursor:
+            _cursor.execute('SELECT "USERNAME", "PASSWORD", "FULLNAME", "EMAIL", "BIRTHDAY" FROM "KL_USERS"')
+            # "Return all rows from a cursor as a dict"
+            _columns = [col[0] for col in _cursor.description]
+            return [
+                dict(zip(_columns, row))
+                for row in _cursor.fetchall()
+            ]
 
-    def getdata(self):
-        self.__connection: pg_connection = connections['postgresql_khoailangdb']
-        self.__cursor = self.__connection.cursor()
-
-        self.__cursor.execute("UPDATE bar SET foo = 1 WHERE baz = %s", [self.baz])
-        self.__cursor.execute("SELECT foo FROM bar WHERE baz = %s", [self.baz])
-        row = self.__cursor.fetchone()
-        return row
+    def getallusers(self):
+        with connections['postgresql_khoailangdb'].cursor() as _cursor:
+            _cursor.execute('SELECT "USERNAME", "PASSWORD", "FULLNAME", "EMAIL", "BIRTHDAY" FROM "KL_USERS"')
+            # "Return all rows from a cursor as a namedtuple"
+            _desc = _cursor.description
+            nt_result = namedtuple('Result', [col[0] for col in _desc])
+            return [nt_result(*row) for row in _cursor.fetchall()]
